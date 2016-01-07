285107bf9b26505b3b08080fc67623ca891d1583
Stephen King <stephen@rocketmade.com>
2016-01-06 20:10:38 -0700

    initial set up for adding messages to MessageList componenet from MessageBox component

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import mui from 'material-ui';
+import trim from 'trim';
 
 var {Card} = mui;
 
@@ -10,12 +11,35 @@ class MessageBox extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      message: ''
+    }
+  }
+  
+  onChange(event) {
+    this.setState({
+      message: event.target.value
+    });
+  }
+  
+  onKeyUp(event) {
+    if (event.keyCode === 13 && trim(event.target.value) != '') {
+      event.preventDefault();
+      this.setState({
+        message: ''
+      });
+      console.log('message sent');
+    }
   }
   
   render() {
     return(
       <Card className='message-box'>
-        <textarea />
+        <textarea 
+        value={this.state.message}
+        onChange={this.onChange.bind(this)} // have to .bind(this) for ES6 class, sort of dumb, but whatever
+        onKeyUp={this.onKeyUp.bind(this)}
+        />
       </Card>
     );
   }
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -19,7 +19,22 @@ class MessageList extends React.Component {
     
     this.firebaseRef = new Firebase('https://chatty-chatty.firebaseio.com/messages');
     this.firebaseRef.once('value', (dataSnapShot) => {
-      var messages = dataSnapShot.val();
+    // The data coming from firebase will change when the keys for the data are not
+    // able to be used as array indexes; ie 0,1,2,etc.  Instead it will send objects.
+    // messagesVal is the object it will send back instead of an array.
+      var messagesVal = dataSnapShot.val();
+      // Using lodash to create an array out of the objects pass from firebase for the 
+      // app to use.
+      var messages = _(messagesVal)
+        // Creates array of keys
+        .keys()
+        // Maps over the keys.  .clone creates a clone of the data so it is treated as immutable.
+        .map((messageKey) => {
+          var cloned = _.clone(messagesVal[messageKey]);
+          cloned.key = messageKey;
+          return cloned;
+        }).value();
+        
       this.setState({
         messages: messages
       });
